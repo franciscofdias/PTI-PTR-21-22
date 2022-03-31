b915a2ea05da8cf1fd40c0334a49957697c0f19d
Santos <Santos@TEAM_THE_SANTOS>
2022-03-31 21:51:48 +0100

    Add project files.

---
+++ testproject_ptiptr/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+let name:string
+
+name = "pyush"
+
+function App() {
+  return (
+  <>
+  <h1> helloworld </h1>
+
+   <ul>
+   <li> name </li>
+   </ul>
+
+   </>
+  );
+}
+
+export default App;
