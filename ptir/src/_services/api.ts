4aa7143babb59c09602f02e0ad04095a17a833ed
Gonçalo Rocha <65404659+fc51202@users.noreply.github.com>
2022-04-03 12:56:47 +0100

    fix and added a web route

--- ptir/src/_services/api.ts
+++ ptir/src/_services/api.ts
@@ -1,11 +1,13 @@
 // Axios requests go here
 import axios, { AxiosRequestConfig } from 'axios'
+import { Cookies } from 'react-cookie';
 
-let dev = 'http://localhost:3000'
-let web = 'tbd'
+const dev = 'http://localhost:8000';
+const web = 'https://ptir.pt';
+const cookies = new Cookies();
 
 let config: AxiosRequestConfig = {
-    baseURL: dev,
+    baseURL: web,
     timeout: 10000,
     responseType: 'json',
     validateStatus: (status: number) => status >= 200 && status < 300,
@@ -26,4 +28,42 @@ export const SignUp = (username: string, email: string, password: string, accoun
     formData.append('password', password)
     formData.append('accountType', accountType)
     return axios.post('/api/auth/signup', formData, config)
+}
+
+export const Recovery = (email: string) => {
+    const formData = new FormData()
+    formData.append('email', email)
+    return axios.post('/api/auth/recovery', formData, config)
+}
+
+export const Reset = () => {
+    const formData = new FormData()
+    return axios.post('/api/auth/reset', formData, config)
+}
+
+export const Logout = () => {
+    const formData = new FormData()
+    return axios.post('/api/auth/logout', formData, config)
+}
+
+export const Refresh = () => {
+    const formData = new FormData()
+    return axios.post('/api/auth/refresh', formData, config) 
+}
+
+export const Me = () => {
+    return axios.get('/api/auth/me', config)
+}
+
+export const UpdateUser = () => {
+    const formData = new FormData()
+    return axios.post('/api/auth/updateUser', formData, config)
+}
+
+export const CheckToken = () => {
+    let token = cookies.get("GreenMarketToken")
+    config.headers = {
+        Authorization: "Bearer " + token
+    }
+    return axios.get('/api/auth/checkToken', config)
 }
